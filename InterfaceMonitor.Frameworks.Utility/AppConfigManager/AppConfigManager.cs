05f8e4ba7e7e84be80e1315804f41176a7da876c
WUWEI <wuw_sh@aliyun.com>
2018-05-28 09:21:58 +0800

    commit

---
+++ InterfaceMonitor.Frameworks.Utility/AppConfigManager/AppConfigManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceMonitor.Frameworks.Utility
+{
+    /// <summary>
+    /// Description:应用程序配置管理帮助类
+    /// Author:WUWEI
+    /// Date:2018/05/28
+    /// </summary>
+    public class AppConfigManager
+    {
+        private static IDictionary<string, string> _dicDataProvider;
+        public static IDictionary<string, string> DicDataProvider
+        {
+            get
+            {
+                if (_dicDataProvider == null || _dicDataProvider.Count == 0)
+                {
+                    _dicDataProvider = new Dictionary<string, string>();
+                }
+                return _dicDataProvider;
+            }
+        }
+        public static bool GetDataProvider(string dpname, out string dllname, out string assname)
+        {
+            dllname = DicDataProvider[dpname];
+            assname = dllname.Substring(0, dllname.LastIndexOf("."));
+            return true;
+        }
+    }
+}
