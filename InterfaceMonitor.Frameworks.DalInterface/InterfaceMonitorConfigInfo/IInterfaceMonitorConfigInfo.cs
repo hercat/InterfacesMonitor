cb4c8465c8f244c0b5c82ee867f1b911799e3892
WUWEI <wuw_sh@aliyun.com>
2018-05-28 08:39:20 +0800

    commit

--- InterfaceMonitor.Frameworks.DalInterface/InterfaceMonitorConfigInfo/IInterfaceMonitorConfigInfo.cs
+++ InterfaceMonitor.Frameworks.DalInterface/InterfaceMonitorConfigInfo/IInterfaceMonitorConfigInfo.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using InterfaceMonitor.Frameworks.Entity;
 
 namespace InterfaceMonitor.Frameworks.DalInterface
 {
-    public class Class1
+    public interface IInterfaceMonitorConfigInfo
     {
+        
     }
 }
