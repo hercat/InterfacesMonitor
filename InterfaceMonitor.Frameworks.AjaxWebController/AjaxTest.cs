fec2437794b82827b4c04e12509314b49dd58191
WUWEI <wuw_sh@aliyun.com>
2018-06-07 16:40:15 +0800

    commit

---
+++ InterfaceMonitor.Frameworks.AjaxWebController/AjaxTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyMVC;
+
+namespace InterfaceMonitor.Frameworks.AjaxWebController
+{
+    public class AjaxTest
+    {
+        [Action]
+        public string TestMethod()
+        {
+            return "这是一个测试方法！";
+        }
+    }
+}
