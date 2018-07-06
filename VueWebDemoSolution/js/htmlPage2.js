74ca215f844b2fadf974335f0d4971974c5ba8b0
WUWEI <wuw_sh@aliyun.com>
2018-07-06 18:00:47 +0800

    commit

---
+++ VueWebDemoSolution/js/htmlPage2.js
@@ -0,0 +1,84 @@
+﻿/// <reference path="jquery-3.3.1.min.js" />
+/// <reference path="C:\工作\SourceCode\InterfaceMonitorSolution\VueWebDemoSolution\vue2.5.16/vue.js" />
+
+$(document).ready(function () {
+    //响应式修改数据
+    var app = new Vue({
+        el: '#app',
+        data: {
+            message: 'hello vue',
+            message2:'wuwei'
+        }
+    });
+    //指令
+    var app2 = new Vue({
+        el: '#app2',
+        data: {
+            message: new Date().toLocaleString()
+        }
+    });
+    //条件
+    var app3 = new Vue({
+        el: '#app3',
+        data: {
+            seen: true
+        }
+    });
+    //循环
+    var app4 = new Vue({
+        el: '#app4',
+        data: {
+            todos: [
+                { text: 'C#' },
+                { text: 'Java' },
+                { text: 'PHP' },
+                { text: 'Python' },
+                { text: 'C++' }
+            ]
+        }
+    });
+    var app4_2 = new Vue({
+        el: '#app4-2',
+        data: {
+            list: [
+                { name: 'Bob' },
+                { name: 'John' },
+                { name: 'Jack' },
+                { name: 'Steven' }
+            ]
+        }
+    });
+    //处理用户输入
+    var app5 = new Vue({
+        el: '#app5',
+        data: {
+            message: 'hello vue.js'
+        },
+        methods: {
+            reverseMessage: function () {
+                this.message = this.message.split('').reverse().join('')
+            }
+        }
+    });
+    var app5_2 = new Vue({
+        el: '#app5-2',
+        data: {
+            message: 'hello vue.js'
+        }
+    });
+    Vue.component('todo-item', {
+        props: ['todo'],
+        template: '<li>{{ todo.text }}</li>'
+    });
+
+    var app7 = new Vue({
+        el: '#app-7',
+        data: {
+            groceryList: [
+              { id: 0, text: '蔬菜' },
+              { id: 1, text: '奶酪' },
+              { id: 2, text: '随便其它什么人吃的东西' }
+            ]
+        }
+    });
+});
---
+++ VueWebDemoSolution/js/htmlPage3.js
@@ -0,0 +1,44 @@
+﻿/// <reference path="jquery-3.3.1.min.js" />
+/// <reference path="C:\工作\SourceCode\InterfaceMonitorSolution\VueWebDemoSolution\vue2.5.16/vue.js" />
+
+$(document).ready(function () {
+
+    //var data = { a: 1 };
+    //var vm = new Vue({
+    //    data: data
+    //});
+    //alert(vm.a == data.a);
+    //alert("修改vm.a=2前,vm.a:" + vm.a + ",data.a:" + data.a);
+    //vm.a = 2;
+    //alert("修改vm.a=2后,vm.a:" + vm.a + ",data.a:" + data.a);
+    //data.a = 3;
+    //alert("修改data.a=3后,vm.a:" + vm.a + ",data.a:" + data.a);
+    //vm.b = "hi";
+    //alert("添加vm.b='hi'后,vm.b:" + vm.b + ",data.b:" + data.b);
+
+    //var obj = { foo: 'bar' };
+    //Object.freeze(obj);
+    //new Vue({
+    //    el: '#app',
+    //    data: obj,
+    //    methods: {
+    //        changeIt: function () {
+    //            data.foo = 'baz'
+    //        }
+    //    }
+    //});
+
+    var vm = new Vue({
+        data: {
+            a: 1
+        },
+        created: function () {
+            console.log('a is:' + this.a);
+        }
+    });
+    vm.$watch('a', function (newValue, oldValue) {
+        console.log('oldValue is:' + oldValue + ",newValue is:" + newValue);
+    });
+    vm.a = 3;
+
+});
