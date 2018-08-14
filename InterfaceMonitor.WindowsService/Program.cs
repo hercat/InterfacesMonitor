037f3bfc089f5451cd18dc569f11339af99a146f
WUWEI <wuw_sh@aliyun.com>
2018-08-14 18:08:53 +0800

    commit

--- InterfaceMonitor.WindowsService/Program.cs
+++ InterfaceMonitor.WindowsService/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
+using log4net.Config;
 
 namespace InterfaceMonitor.WindowsService
 {
@@ -14,6 +15,9 @@ namespace InterfaceMonitor.WindowsService
         /// </summary>
         static void Main()
         {
+            string currentPath = AppDomain.CurrentDomain.BaseDirectory;
+            string log4netConfig = string.Format("{0}\\log4net.xml", currentPath);
+            XmlConfigurator.ConfigureAndWatch(new System.IO.FileInfo(log4netConfig));
             ServiceBase[] ServicesToRun;
             ServicesToRun = new ServiceBase[]
             {
---
+++ VueWebDemoSolution/js/htmlPage4.js
@@ -0,0 +1,9 @@
+﻿$(function () {
+    var app = new Vue({
+        el: '#app',
+        data: {
+            header:'This is title',
+            message: 'hello Vue.js'
+        }
+    });
+});
